8de0a7b0560236417aad0dbf5592a1d7507635e5
Luca Innocenti <lukeinnocenti@gmail.com>
2017-03-11 21:30:50 +0000

    added PacletInfo.m

---
+++ PacletInfo.m
@@ -0,0 +1,13 @@
+(* ::Package:: *)
+
+Paclet[
+	Name -> "QM",
+	Version -> "0.1",
+	WolframVersion -> "11.0+",
+	Description -> "A framework for quantum mechanical calculations.",
+	Creator -> "Luca Innocenti",
+	Extensions -> {
+		{"Kernel", Root -> ".", Context -> "QM"}(*,
+		{"Documentation", Language \[Rule] "English", MainPage \[Rule] "Guides/QM"}*)
+	}
+]
