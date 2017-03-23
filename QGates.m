b3515f51168db6aae5b5b592f8baa8681058cef0
Luca Innocenti <lukeinnocenti@gmail.com>
2017-03-23 14:39:16 +0000

    better handling of general 1-qubit gates

--- QGates.m
+++ QGates.m
@@ -21,9 +21,9 @@ PauliX;
 PauliY;
 PauliZ;
 
-TwoQubitGate;
+TwoQubitGate::usage = "\
+TwoQubitGate[numQubits, control, target, matrix] returns the matrix representing the gate `matrix` between the control and the target qubit.";
 Toffoli;
-
 
 Begin["`Private`"];
 
@@ -39,75 +39,6 @@ p11 = ProjectionMatrix[1, 1, 1];
 p22 = ProjectionMatrix[1, 2, 2];
 
 
-TwoQubitGate[numQubits_Integer, control_Integer, target_Integer, matrix_] := Block[
-  {tp, matrixAsTP, transposedBigTP},
-(* Initialize a list of 2 dimensional identity matrices *)
-  matrixAsTP = Transpose[ArrayReshape[matrix, {2, 2, 2, 2}], {1, 3, 2, 4}];
-  tp = TensorProduct[
-    TensorProduct @@ ConstantArray[IdentityMatrix @ 2, numQubits - 2],
-    matrixAsTP
-  ];
-  transposedBigTP = Transpose[tp,
-    Sequence @@ {2 # - 1, 2 #} & /@
-        {Sequence @@ Complement[Range @ numQubits, {control, target}], control, target}
-  ];
-
-  Flatten[transposedBigTP,
-    {
-      Range[1, 2 numQubits, 2],
-      Range[2, 2 numQubits, 2]
-    }
-  ]
-];
-
-(* CPhase is the controlled phase gate *)
-CPhase[numQubits_Integer, control_Integer, target_Integer] := Block[
-  {identities1, identities2},
-(* Initialize a list of 2 dimensional identity matrices *)
-  identities1 = identities2 = ConstantArray[
-    IdentityMatrix @ 2, numQubits
-  ];
-  (* Replace values where appropriate *)
-  identities1[[control]] = p11;
-  identities2[[control]] = p22;
-  identities2[[target]] = PauliMatrix @ 3;
-  (* And finally return the gate: *)
-  KroneckerProduct @@ identities1 +
-      KroneckerProduct @@ identities2
-];
-CPhase[numQubits_, {control_, target_}] := CPhase[
-  numQubits, control, target
-];
-CPhase[control_, target_] := CPhase[
-  2, control, target
-];
-CPhase[] := CPhase[2, 1, 2];
-
-
-(* `CNot` is the controlled-not gate *)
-CNot[numQubits_Integer, control_Integer, target_Integer] := Block[
-  {identities1, identities2},
-(* Initialize a list of 2 dimensional identity matrices *)
-  identities1 = identities2 = ConstantArray[
-    IdentityMatrix @ 2, numQubits
-  ];
-  (* Replace values where appropriate *)
-  identities1[[control]] = p11;
-  identities2[[control]] = p22;
-  identities2[[target]] = PauliMatrix @ 1;
-  (* And finally return the gate: *)
-  KroneckerProduct @@ identities1 +
-      KroneckerProduct @@ identities2
-];
-CNot[numQubits_, {control_, target_}] := CNot[
-  numQubits, control, target
-];
-CNot[control_, target_] := CNot[
-  2, control, target
-];
-CNot[] := CNot[2, 1, 2];
-
-
 OneQubitGate[numQubits_Integer, target_Integer, matrix_] := Block[
   {identities},
   identities = ConstantArray[
@@ -118,6 +49,37 @@ OneQubitGate[numQubits_Integer, target_Integer, matrix_] := Block[
   (* Return result *)
   KP @@ identities
 ];
+
+
+(* To compute a two qubit gate between any pair of qubits,
+   we first write the matrix describing the two-qubit gate
+   acting on the last two qubits, and then permute the indices
+   appropriately.*)
+TwoQubitGate[numQubits_Integer, control_Integer, target_Integer, matrix_] := Block[
+  {tp, matrixAsTP, transposedBigTP},
+(* Initialize a list of 2 dimensional identity matrices *)
+  matrixAsTP = Transpose[ArrayReshape[matrix, {2, 2, 2, 2}], {1, 3, 2, 4}];
+  tp = TensorProduct[
+    TensorProduct @@ ConstantArray[IdentityMatrix @ 2, numQubits - 2],
+    matrixAsTP
+  ];
+  transposedBigTP = Transpose[tp,
+    Sequence @@ {2 # - 1, 2 #} & /@
+        {
+          Sequence @@ Complement[Range @ numQubits, {control, target}],
+          control,
+          target
+        }
+  ];
+  (* Rearrange indices and flatten TensorProduct into a KroneckerProduct structure *)
+  Flatten[transposedBigTP,
+    {
+      Range[1, 2 numQubits, 2],
+      Range[2, 2 numQubits, 2]
+    }
+  ]
+];
+
 
 Hadamard[numQubits_Integer, target_Integer] := OneQubitGate[numQubits, target, HadamardMatrix[2]];
 Hadamard[numQubits_, {target_}] := Hadamard[numQubits, target];
@@ -141,14 +103,67 @@ PauliZ[target_] := PauliZ[1, 1];
 PauliZ[] = PauliZ[1, 1];
 
 
-(* Three qubits gates *)
-Toffoli[] := None;
+(* -------- TWO QUBIT GATES -------- *)
+
+
+(* CPhase is the controlled phase gate *)
+CPhase[] := Plus[
+  KP[{{1, 0}, {0, 0}}, IdentityMatrix @ 2],
+  KP[{{0, 0}, {0, 1}}, PauliZ[]]
+];
+CPhase[numQubits_Integer, control_Integer, target_Integer] := TwoQubitGate[
+  numQubits, control, target, CPhase[]
+];
+CPhase[numQubits_, {control_, target_}] := CPhase[
+  numQubits, control, target
+];
+CPhase[control_, target_] := CPhase[
+  2, control, target
+];
+
+
+(* `CNot` is the controlled-not gate *)
+CNot[] := Plus[
+  KP[{{1, 0}, {0, 0}}, IdentityMatrix @ 2],
+  KP[{{0, 0}, {0, 1}}, PauliX[]]
+];
+CNot[numQubits_Integer, control_Integer, target_Integer] := TwoQubitGate[
+  numQubits, control, target, CNot[]
+];
+CNot[numQubits_, {control_, target_}] := CNot[
+  numQubits, control, target
+];
+CNot[control_, target_] := CNot[
+  2, control, target
+];
+
+
+(* -------- THREE QUBIT GATES -------- *)
+
+
+Toffoli[] := SparseArray[
+  {
+    {i_, i_} /; i <= 6 -> 1,
+    {7, 8} -> 1, {8, 7} -> 1
+  },
+  {8, 8}
+];
+
+
+Fredkin[] := SparseArray[
+  {
+    {i_, i_} /; i < 8 -> 1,
+    {8, 8} -> -1
+  },
+  {8, 8}
+];
+
 
 
 (* Protect all package symbols *)
-    With[{syms = Names["QM`QGates`*"]},
-      SetAttributes[syms, {Protected, ReadProtected}]
-    ];
+With[{syms = Names["QM`QGates`*"]},
+  SetAttributes[syms, {Protected, ReadProtected}]
+];
 
 
 End[];
